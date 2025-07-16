1d36f2098bc999c28e67d0140bc083639e82842f
shankar samidala <samidalashankar@gmail.com>
2025-07-16 09:23:57 +0530

    new

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: '.next',
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
 };
 
 module.exports = nextConfig;
